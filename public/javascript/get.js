1a244ed8f45ad833371218a239c8eee6096b8a33
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-08-03 16:40:54 -0400

    way neater

--- public/javascript/get.js
+++ public/javascript/get.js
@@ -1,9 +1,6 @@
 var axios = require("axios").default;
 
-let lat;
-let lon;
-
-function getRestaurants() {
+async function getRestaurants(lat, lon) {
     console.log('getting restaurants')
     var restaurants = {
         method: 'GET',
@@ -25,43 +22,49 @@ function getRestaurants() {
     };
 
     axios.request(restaurants).then(async function (response) {
-        let cuisines = {}
+        let cuisines = []
 
         for (let i = 0; i < response.data.data.length; i++) {
             let restaurant = await response.data.data[i];
-            // console.log(restaurant.name)
-            // console.log(restaurant.cuisine)
 
-            // (Object.values(restaurant));
-            if (restaurant.cuisine){
-                for (let j = 0; j < restaurant.cuisine.length; j++) {
-                    console.log(JSON.stringify(restaurant.cuisine[j].name));
-                    cuisines[JSON.stringify(restaurant.cuisine[j].name)] = '';
+            if (restaurant.name) { // if the restaurant has a name 
+                if (restaurant.cuisine) { // and also has a cuisine listed
+                    for (let j = 0; j < restaurant.cuisine.length; j++) {
+                        if (cuisines.includes(restaurant.cuisine[j].name) === false) { // allow no duplicates
+                            cuisines.push(restaurant.cuisine[j].name); // add cuisines
+                        }
+                    }
                 }
-                // try .filter()
+                console.log(i + ": " + restaurant.name) // just for show .. log names with their index number
             }
         }
-        console.log(cuisines)
+        console.log(cuisines) // log filled cuisines array
     }).catch(function (error) {
         console.error(error);
     });
 }
 
-console.log('getting location')
-var location = {
-    method: 'GET',
-    url: 'https://ip-geo-location.p.rapidapi.com/ip/check',
-    params: { format: 'json' },
-    headers: {
-        'x-rapidapi-key': 'ac95e95c1dmsh4614f2ca3bc50acp1b7076jsnc9412c4bada5',
-        'x-rapidapi-host': 'ip-geo-location.p.rapidapi.com'
-    }
-};
-axios.request(location).then(async function (response) {
-    lat = await response.data.location.latitude;
-    lon = await response.data.location.longitude;
-    console.log(lat, lon)
-    getRestaurants()
-}).catch(function (error) {
-    console.error(error);
-})
+function getLocation() {
+    var location = {
+        method: 'GET',
+        url: 'https://ip-geo-location.p.rapidapi.com/ip/check',
+        params: { format: 'json' },
+        headers: {
+            'x-rapidapi-key': 'ac95e95c1dmsh4614f2ca3bc50acp1b7076jsnc9412c4bada5',
+            'x-rapidapi-host': 'ip-geo-location.p.rapidapi.com'
+        }
+    };
+    console.log('getting location')
+    // Awaits coordinates then sends them to getRestaurants function
+    axios.request(location).then(async function (response) {
+        let lat = await response.data.location.latitude;
+        let lon = await response.data.location.longitude;
+        console.log(lat, lon)
+        getRestaurants(lat, lon)
+    }).catch(function (error) {
+        console.log('No coordinates could be recieved')
+        console.error(error);
+    })
+}
+
+getLocation()
