7431a320c8375f4b87f021ceafdace17177a3010
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-08-05 19:56:03 -0400

    site hosted with pages linked

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,16 +3,24 @@ const router = require('express').Router();
 const { User, Cuisine } = require('../models');
 
 router.get('/', (req, res) => {
-    console.log('======================');
-    res.render('homepage');
+  console.log('======================');
+  res.render('landing-page');
 })
 
 router.get('/login', (req, res) => {
-    // if (req.session.loggedIn) {
-    //   res.redirect('/');
-    //   return;
-    // }
-    res.render('login');
-  })
+  // if (req.session.loggedIn) {
+  //   res.redirect('/');
+  //   return;
+  // }
+  res.render('login');
+})
+
+router.get('/history', (req, res) => {
+  // if (req.session.loggedIn) {
+  //   res.redirect('/');
+  //   return;
+  // }
+  res.render('history');
+})
 
 module.exports = router;
